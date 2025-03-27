461bf6e81bc82fdbbde923a701626b6b8b9f43df
PivoWarZ <academik87@gmail.com>
2025-03-27 22:43:14 +0300

    - Serializefield in Bullet Config

--- Assets/Scripts/Bullets/BulletConfig.cs
+++ Assets/Scripts/Bullets/BulletConfig.cs
@@ -9,9 +9,9 @@ namespace ShootEmUp
 
     public sealed class BulletConfig : ScriptableObject
     {
-        [SerializeField] public PhysicsLayer PhysicsLayer;
-        [SerializeField] public Color Color;
-        [SerializeField] public int Damage;
-        [SerializeField] public float Speed;
+        public PhysicsLayer PhysicsLayer;
+        public Color Color;
+        public int Damage;
+        public float Speed;
     }
 }
