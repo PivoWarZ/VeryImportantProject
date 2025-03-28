b9c84085fe54a104bb5f7a4fe7164311c3ac9773
Academik <academik87@gmail.com>
2025-03-28 18:25:28 +0300

    BulletSystem refact

--- Assets/Scripts/Bullets/PoolContainer.cs
+++ Assets/Scripts/Bullets/PoolContainer.cs
@@ -6,24 +6,37 @@ namespace ShootEmUp
     public class PoolContainer : MonoBehaviour
     {
         [SerializeField] private Transform _poolContainer;
+        [SerializeField] private Bullet _bulletPrefab;
+        [SerializeField] private Transform _worldTransform;
 
         private readonly Queue<Bullet> _bulletsPool = new();
+
         public Transform GetContainerTransform()
         {
             return this.transform;
         }
 
-        public Bullet TryDequeueBulletInPool()
+        public Transform GetWorldTransform()
         {
-            _bulletsPool.TryDequeue(out Bullet bullet);
-            return bullet;
+            return _worldTransform;
+        }
+
+        public Bullet GetBulletInPool()
+        {
+            if (_bulletsPool.TryDequeue(out Bullet bullet))
+            {
+                return bullet;
+            }
+            else
+            {
+                bullet = Instantiate(_bulletPrefab, _worldTransform);
+                return bullet;
+            }
         }
 
         public void AddBulletInPool(Bullet bullet)
         {
             _bulletsPool.Enqueue(bullet);
         }
-
-
     }
 }
