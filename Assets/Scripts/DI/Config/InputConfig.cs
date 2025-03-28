ea7ba9b3f73fcc1bd4a30f91f400241ca75a7331
Academik <academik87@gmail.com>
2025-03-28 22:59:43 +0300

    not commit

---
+++ Assets/Scripts/DI/Config/InputConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace ShootEmUp
+{ 
+        [Serializable]
+        public class InputConfig : IkeyBoardInputConfig
+        {
+            [SerializeField] private KeyCode _shoot;
+            [SerializeField] private KeyCode _left;
+            [SerializeField] private KeyCode _right;
+
+            public KeyCode Shoot => _shoot;
+            public KeyCode Left => _left;
+            public KeyCode Right => _right;
+        }
+}
